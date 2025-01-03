15e14624e6dc780f9a0c916c827fe09ba4bd2298
Romain <116721352+Romaindujardin@users.noreply.github.com>
2025-01-03 21:20:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -107,108 +107,117 @@ def rag_pipeline(query, k=4):
 
 
 
-# Set page configuration
+
+              
+
+
+        #   # Charger le fichier OBJ directement
+        #   obj_file_path = "test.obj"  # Remplace par le chemin de ton fichier
+        #   try:
+        #       with open(obj_file_path, "r") as file:
+        #           obj_data = file.read()
+        #   except FileNotFoundError:
+        #       st.error(f"Le fichier '{obj_file_path}' est introuvable. Vérifie le chemin.")
+        #       st.stop()
+
+        #   # Extraction des données du fichier OBJ
+        #   vertices = []
+        #   faces = []
+        #   vertex_colors = []  # Pour stocker les couleurs des sommets
+
+        #   for line in obj_data.split("\n"):
+        #       parts = line.strip().split()
+        #       if not parts or parts[0] not in {"v", "f"}:  # Ignorer les lignes vides ou inutiles
+        #           continue
+        #       if parts[0] == "v":  # Vertex avec couleur potentielle
+        #           try:
+        #               if len(parts) >= 4:  # Minimum : v x y z
+        #                   vertices.append([float(parts[1]), float(parts[2]), float(parts[3])])
+        #                   if len(parts) == 7:  # Si les couleurs sont présentes
+        #                       vertex_colors.append([float(parts[4]), float(parts[5]), float(parts[6])])
+        #                   else:
+        #                       vertex_colors.append([1, 1, 1])  # Couleur blanche par défaut
+        #           except ValueError:
+        #               st.warning(f"Ligne invalide ignorée : {line}")
+        #       elif parts[0] == "f":  # Face
+        #           try:
+        #               face = [int(p.split('/')[0]) - 1 for p in parts[1:]]
+        #               faces.append(face)
+        #           except ValueError:
+        #               st.warning(f"Ligne de face invalide ignorée : {line}")
+
+        #   # Vérification des données chargées
+        #   if not vertices or not faces:
+        #       st.error("Impossible de lire correctement les sommets ou les faces depuis le fichier OBJ.")
+        #       st.stop()
+
+        #   # Préparation des données pour Plotly
+        #   x, y, z = zip(*vertices)
+        #   i, j, k = zip(*faces)
+        #   r, g, b = zip(*vertex_colors)
+        #   colors = ['rgb({}, {}, {})'.format(int(r_ * 255), int(g_ * 255), int(b_ * 255)) for r_, g_, b_ in zip(r, g, b)]
+
+        #   # Création de la figure 3D avec couleurs
+        #   fig = go.Figure(data=[
+        #       go.Mesh3d(
+        #           x=x, y=y, z=z,
+        #           i=i, j=j, k=k,
+        #           vertexcolor=colors,  # Ajout des couleurs par sommet
+        #           opacity=1.0,
+        #           hoverinfo="skip",  # Désactive l'affichage des coordonnées au survol
+        #       )
+        #   ])
+
+        #   # Suppression des axes, de la grille et des marges
+        #   fig.update_layout(
+        #       scene=dict(
+        #           xaxis=dict(visible=False),  # Désactive l'axe X
+        #           yaxis=dict(visible=False),  # Désactive l'axe Y
+        #           zaxis=dict(visible=False),  # Désactive l'axe Z
+        #           camera=dict(
+        #               eye=dict(x=0, y=0, z=1.5),  # Position de la caméra
+        #               up=dict(x=1, y=0, z=1),         # Orientation "haut" de la caméra
+        #               center=dict(x=0, y=0, z=0)      # Point vers lequel la caméra regarde
+        #           )
+        #       ),
+        #       margin=dict(l=0, r=0, t=0, b=0),  # Supprime les marges autour de l'objet
+        #   )
+
+        #   # Affichage dans Streamlit
+        #   st.plotly_chart(fig, use_container_width=True)
+        # Afficher l'image A de base
+
 st.set_page_config(layout="wide")
 
-# Initialize session state
-if 'page' not in st.session_state:
-    st.session_state.page = 'home'
+st.markdown(
+        """
+        <style>
+        .centered {
+            text-align: center;
+        }
+        .h1 {
+            font-size: 7vw;
+        }
+        .p {
+            font-size: 1vw;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
-# Navigation
-if st.session_state.page == 'home':
-    import home
-    home.main()
-elif st.session_state.page == 'main':
+st.markdown('<h1 class="centered h1">Welcome to, <span style="opacity: 0.5;">rom</span>A</span>I<span style="opacity: 0.5;">n</span></h1>', unsafe_allow_html=True)
+st.markdown('<p class="centered p">here is <span style="opacity: 0.5;">rom</span>A</span>I<span style="opacity: 0.5;">n</span>, an AI in the image of Romain Dujardin. Ask him questions in English and he will answer them as best he can.</p>', unsafe_allow_html=True)
+st.markdown('<p class="centered p">(Can be made mistake)</p>', unsafe_allow_html=True)
 
-              
+    
+        
+# Champ de texte pour l'utilisateur
+query = st.text_input("Your question:")
 
-
-          # Charger le fichier OBJ directement
-          obj_file_path = "test.obj"  # Remplace par le chemin de ton fichier
-          try:
-              with open(obj_file_path, "r") as file:
-                  obj_data = file.read()
-          except FileNotFoundError:
-              st.error(f"Le fichier '{obj_file_path}' est introuvable. Vérifie le chemin.")
-              st.stop()
-
-          # Extraction des données du fichier OBJ
-          vertices = []
-          faces = []
-          vertex_colors = []  # Pour stocker les couleurs des sommets
-
-          for line in obj_data.split("\n"):
-              parts = line.strip().split()
-              if not parts or parts[0] not in {"v", "f"}:  # Ignorer les lignes vides ou inutiles
-                  continue
-              if parts[0] == "v":  # Vertex avec couleur potentielle
-                  try:
-                      if len(parts) >= 4:  # Minimum : v x y z
-                          vertices.append([float(parts[1]), float(parts[2]), float(parts[3])])
-                          if len(parts) == 7:  # Si les couleurs sont présentes
-                              vertex_colors.append([float(parts[4]), float(parts[5]), float(parts[6])])
-                          else:
-                              vertex_colors.append([1, 1, 1])  # Couleur blanche par défaut
-                  except ValueError:
-                      st.warning(f"Ligne invalide ignorée : {line}")
-              elif parts[0] == "f":  # Face
-                  try:
-                      face = [int(p.split('/')[0]) - 1 for p in parts[1:]]
-                      faces.append(face)
-                  except ValueError:
-                      st.warning(f"Ligne de face invalide ignorée : {line}")
-
-          # Vérification des données chargées
-          if not vertices or not faces:
-              st.error("Impossible de lire correctement les sommets ou les faces depuis le fichier OBJ.")
-              st.stop()
-
-          # Préparation des données pour Plotly
-          x, y, z = zip(*vertices)
-          i, j, k = zip(*faces)
-          r, g, b = zip(*vertex_colors)
-          colors = ['rgb({}, {}, {})'.format(int(r_ * 255), int(g_ * 255), int(b_ * 255)) for r_, g_, b_ in zip(r, g, b)]
-
-          # Création de la figure 3D avec couleurs
-          fig = go.Figure(data=[
-              go.Mesh3d(
-                  x=x, y=y, z=z,
-                  i=i, j=j, k=k,
-                  vertexcolor=colors,  # Ajout des couleurs par sommet
-                  opacity=1.0,
-                  hoverinfo="skip",  # Désactive l'affichage des coordonnées au survol
-              )
-          ])
-
-          # Suppression des axes, de la grille et des marges
-          fig.update_layout(
-              scene=dict(
-                  xaxis=dict(visible=False),  # Désactive l'axe X
-                  yaxis=dict(visible=False),  # Désactive l'axe Y
-                  zaxis=dict(visible=False),  # Désactive l'axe Z
-                  camera=dict(
-                      eye=dict(x=0, y=0, z=1.5),  # Position de la caméra
-                      up=dict(x=1, y=0, z=1),         # Orientation "haut" de la caméra
-                      center=dict(x=0, y=0, z=0)      # Point vers lequel la caméra regarde
-                  )
-              ),
-              margin=dict(l=0, r=0, t=0, b=0),  # Supprime les marges autour de l'objet
-          )
-
-          # Affichage dans Streamlit
-          st.plotly_chart(fig, use_container_width=True)
-
-
-              
-
-              
-
-          # Champ de texte pour l'utilisateur
-          query = st.text_input("Your question:")
-
-          if query:
-                  with st.spinner("Generating the response..."):
-                      answer = rag_pipeline(query)
-                  # Afficher uniquement la réponse
-                  st.subheader("Answer:")
-                  st.write(answer)
+if query:
+    with st.spinner("Generating the response..."):
+        answer = rag_pipeline(query)
+    # Afficher uniquement la réponse
+    st.subheader("Answer:")
+    st.write(answer)
